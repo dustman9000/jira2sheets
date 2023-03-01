4e3bb54e6b1ac2bf1cfcd0bffa3accae8468bd13
Dustin Row <drow@redhat.com>
2023-02-28 19:29:00 -0800

    Update pkg path and remove 'activeSprint' code

--- main.go
+++ main.go
@@ -1,10 +1,9 @@
 /*
 Copyright © 2022 NAME HERE <EMAIL ADDRESS>
-
 */
 package main
 
-import "github.com/bf2fc6cc711aee1a0c2a/jira2sheets/cmd"
+import "github.com/dustman9000/jira2sheets/cmd"
 
 func main() {
 	cmd.Execute()
